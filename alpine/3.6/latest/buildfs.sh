5db16bf6e0c146e9babcab2e6182a456b4ad27c4
Stefan <mail.ipv4v6+gh@gmail.com>
2018-01-16 21:59:16 +0100

    add Alpine 3.6

---
+++ alpine/3.6/latest/buildfs.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+DIST=alpine
+DVER=3.6
+DATE=latest
+BUILDROOT=~/BUILDROOT
+TARGET=${BUILDROOT}/${DIST}-${DVER}-${DATE}
+MIRROR=http://dl-cdn.alpinelinux.org/alpine
+PUBKEY_BASEURL=https://alpinelinux.org/keys/
+PUBKEY_FILE="alpine-devel@lists.alpinelinux.org-4a6a0840.rsa.pub"
+APK=~/bin/apk.static
+APK_SHA256=96226e5edefd0a46fb471a12fc8e7e0034c466e1da89676945e64552e3cd0be5
+APKTOOL_BASEURL=${MIRROR}/v${DVER}/main/x86_64/
+APKTOOL_FILE=apk-tools-static-2.7.5-r0.apk
+APKTOOL_FILE_SHA256=5b063627faf59c41f883cbdccfd7c4be85acb36681a749c02513644a7166455a
+
+rm -rf ${TARGET}
+mkdir -p ${TARGET}/etc/apk/keys
+if [ ! -f ${APK} ] ;
+then
+	wget ${APKTOOL_BASEURL}${APKTOOL_FILE}
+	echo "${APKTOOL_FILE_SHA256}  ${APKTOOL_FILE}" | sha256sum -c -
+	mkdir -p ~/bin
+	tar -C ~/bin --strip-components=1 -xzf ${APKTOOL_FILE} sbin/apk.static
+	rm ${APKTOOL_FILE}
+fi
+echo "${APK_SHA256}  ${APK}" | sha256sum -c -
+wget ${PUBKEY_BASEURL}${PUBKEY_FILE}
+mv -i ${PUBKEY_FILE} ${TARGET}/etc/apk/keys/
+${APK} -X ${MIRROR}/v${DVER}/main -U --root ${TARGET} --initdb add alpine-base
+echo ${MIRROR}/v${DVER}/main > ${TARGET}/etc/apk/repositories
+echo ${DIST} > ${TARGET}/etc/hostname
+echo "nameserver 8.8.8.8" > ${TARGET}/etc/resolv.conf
+rm -rf ${TARGET}/dev/*
+rm -rf ${TARGET}/var/cache/apk/*
+tar -cJvf ./rootfs-${DIST}-${DVER}-${DATE}.tar.xz -C ${TARGET} .
