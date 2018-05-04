0f10ab9b730c548182142ad76441f00f134b728e
Stefan <mail.ipv4v6+gh@gmail.com>
2018-05-04 22:34:42 +0200

    add Fedora 28

---
+++ fedora/28/latest/buildfs.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+DIST=fedora
+DVER=28
+DATE=latest
+BUILDROOT=~/BUILDROOT
+TARGET=${BUILDROOT}/${DIST}-${DVER}-${DATE}
+
+rm -rf ${TARGET}
+mkdir -p ${TARGET}/etc
+
+cat << END > ${TARGET}/etc/yum.conf
+[main]
+
+[fedora]
+name=fedora
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-${DVER}&arch=x86_64
+gpgkey=https://getfedora.org/static/9DB62FB1.txt
+gpgcheck=1
+
+[updates]
+name=updates
+metalink=https://mirrors.fedoraproject.org/metalink?repo=updates-released-f${DVER}&arch=x86_64
+gpgkey=https://getfedora.org/static/9DB62FB1.txt
+gpgcheck=1
+END
+
+echo "%_dbpath /var/lib/rpm" > ~/.rpmmacros
+yum -y --installroot ${TARGET} install bash coreutils dnf rpm
+echo "nameserver 8.8.8.8" > ${TARGET}/etc/resolv.conf
+rm -rf ${TARGET}/dev/*
+rm -rf ${TARGET}/var/cache/yum
+rm -rf ${TARGET}/var/lib/yum
+tar -cJvf ./rootfs-${DIST}-${DVER}-${DATE}.tar.xz -C ${TARGET} .
