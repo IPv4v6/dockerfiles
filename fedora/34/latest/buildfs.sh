31bff7c363b3ad8715a7b400f82c1ee27f550762
Stefan <mail.ipv4v6+gh@gmail.com>
2022-01-13 23:15:00 +0100

    add Fedora 34

---
+++ fedora/34/latest/buildfs.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+DIST=fedora
+DVER=34
+DATE=latest
+BUILDROOT=~/BUILDROOT
+TARGET=${BUILDROOT}/${DIST}-${DVER}-${DATE}
+
+rm -rf ${TARGET}
+mkdir -p ${TARGET}/etc/yum.repos.d
+
+cat << END > ${TARGET}/etc/yum.repos.d/fedora.repo
+[fedora]
+name=fedora
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-${DVER}&arch=x86_64
+gpgkey=https://de.pgpkeys.eu/pks/lookup?op=get&search=0x8c5ba6990bdb26e19f2a1a801161ae6945719a39
+gpgcheck=1
+END
+
+cat << END > ${TARGET}/etc/yum.repos.d/fedora-updates.repo
+[updates]
+name=updates
+metalink=https://mirrors.fedoraproject.org/metalink?repo=updates-released-f${DVER}&arch=x86_64
+gpgkey=https://de.pgpkeys.eu/pks/lookup?op=get&search=0x8c5ba6990bdb26e19f2a1a801161ae6945719a39
+gpgcheck=1
+END
+
+echo "%_dbpath /var/lib/rpm" > ~/.rpmmacros
+echo "%_db_backend sqlite" >> ~/.rpmmacros
+dnf -y --installroot ${TARGET} --releasever ${DVER} --exclude "systemd" install bash.x86_64 coreutils dnf rpm glibc-minimal-langpack
+echo "nameserver 8.8.8.8" > ${TARGET}/etc/resolv.conf
+rm -rf ${TARGET}/dev/*
+rm -rf ${TARGET}/var/cache/dnf
+tar -cJvf ./rootfs-${DIST}-${DVER}-${DATE}.tar.xz -C ${TARGET} .
